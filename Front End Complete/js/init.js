5e84a02c9b9d3d32cf63d77ccbc1163158d4de50
Carlo julio <Carlojc@users.noreply.github.com>
2019-07-02 10:32:14 +0700

    Front End Ver 1.0

---
+++ Front End Complete/js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.collapsible').collapsible();
+    $('select').formSelect();
+    $('.datepicker').datepicker();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
