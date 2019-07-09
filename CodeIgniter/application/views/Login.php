3d7b242b459d1b8f0d5f0a11ea891e2f672d278e
Carlo julio <Carlojc@users.noreply.github.com>
2019-07-09 13:57:35 +0700

    direktori asset

--- CodeIgniter/application/views/Login.php
+++ CodeIgniter/application/views/Login.php
@@ -10,10 +10,10 @@
 
         <!-- CSS -->
         <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
-        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
-		<link rel="stylesheet" href="assets/css/form-elements.css">
-        <link rel="stylesheet" href="assets/css/login_css.css">
+        <link rel="stylesheet" href="<?=base_url('assets')?>/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?=base_url('assets')?>/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="<?=base_url('assets')?>/css/form-elements.css">
+        <link rel="stylesheet" href="<?=base_url('assets')?>/css/login_css.css">
 
     </head>
     <body>
@@ -62,10 +62,10 @@
 
 
         <!-- Javascript -->
-        <script src="assets/js/jquery-1.11.1.min.js"></script>
-        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-        <script src="assets/js/jquery.backstretch.min.js"></script>
-        <script src="assets/js/scripts.js"></script>
+        <script src="<?=base_url('assets')?>/js/jquery-1.11.1.min.js"></script>
+        <script src="<?=base_url('assets')?>/bootstrap/js/bootstrap.min.js"></script>
+        <script src="<?=base_url('assets')?>/js/jquery.backstretch.min.js"></script>
+        <script src="<?=base_url('assets')?>/js/scripts.js"></script>
         
    
     </body>
